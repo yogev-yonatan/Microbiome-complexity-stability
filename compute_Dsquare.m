2199c34c2fedf44545cf423ef5d44446b16955cb
yogev-yonatan <58689264+yogev-yonatan@users.noreply.github.com>
2022-03-15 15:13:54 +0200

    Add files via upload

---
+++ compute_Dsquare.m
@@ -0,0 +1,24 @@
+function D2 = compute_Dsquare(overlap,dissimilarity,p)
+
+% This function computes D^2 from DOC
+% Yogev Yonatan, yogev.yn@gmail.com
+% 
+% Inputs:
+% overlap - vector of overlap values
+% dissimilarity - vector of dissimilarity values
+% p - the fraction of data points for D^2 computation (based on the highers
+% overlap values' data)
+
+% Output:
+% D2 - the D^2 value
+
+[overlap,i]=sort(overlap,'ascend');
+dissimilarity=dissimilarity(i);
+
+
+l = length(overlap);
+idx = round(l*p):l;
+
+f = fit(overlap(idx),dissimilarity(idx),'poly1');
+D2 = (f.p1)^2;
+
